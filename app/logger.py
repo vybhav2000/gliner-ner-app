a14f1c8d7c4f575958c6cf7222f4482ddb140f6e
Vybhav Nath <vybhav.nca@gmail.com>
2025-01-05 00:33:55 +0530

    Adding 1 more comment

--- app/logger.py
+++ app/logger.py
@@ -5,4 +5,5 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S,%f"
 )
 
+# getting the uvicorn logger
 logger = logging.getLogger("uvicorn")
